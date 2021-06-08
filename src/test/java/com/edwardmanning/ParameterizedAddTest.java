cee0f47c35303193ae082504ee80dce60ee2d1d8
Edward Manning <edward_manning@ajg.com>
2021-06-08 15:46:35 +0100

    5

--- src/test/java/com/edwardmanning/ParameterizedAddTest.java
+++ src/test/java/com/edwardmanning/ParameterizedAddTest.java
@@ -1,6 +1,5 @@
 package com.edwardmanning;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -11,26 +10,38 @@ import java.util.Collection;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
-public class ParameterizedAddTest extends ParameterizedAbstract {
+public class ParameterizedAddTest {
+    private final MathDTO dto;
 
     @Parameterized.Parameters()
     public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {
-                {3, 2, 5},
-                {2, 3, 5},
-                {1, 4, 5},
-                {0, 5, 5},
-                {-10, 15, 5}
+        return Arrays.asList(new Object[][]{
+                {new MathDTO(3, 2, 5)},
+                {new MathDTO(2, 3, 5)},
+                {new MathDTO(1, 4, 5)},
+                {new MathDTO(0, 5, 5)},
+                {new MathDTO(-10, -15, 5)}
         });
     }
 
-    public ParameterizedAddTest(int first, int second, int result) {
-        super(first, second, result);
+    public ParameterizedAddTest(MathDTO dto) {
+        this.dto = dto;
     }
 
     @Test
     public void add() {
-        //passes
-        assertEquals(result, Math.add(first, second));
+        assertEquals(dto.result, Math.add(dto.first, dto.second));
+    }
+
+    public static class MathDTO {
+        private final int first;
+        private final int second;
+        private final int result;
+
+        public MathDTO(int first, int second, int result) {
+            this.first = first;
+            this.second = second;
+            this.result = result;
+        }
     }
 }
